e6cba36d383eae40afd3016c63ff3523f870ce25
Zer0Stra1n <sandor.seth@gmail.com>
2021-03-21 18:34:32 -0400

    board notes to verify with someone

--- src/game/board/board.tsx
+++ src/game/board/board.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 export const Board: React.FC<{onClick: Function}> = (props: {onClick: Function}) => {
     // the board is 4x4
-    const squares = [Array(16).fill(null)]
+    // TODO: Figure out what this actually looks like from a game perspective :/
+    const squares = [
+        Array(4).fill(null),
+        Array(4).fill(null),
+        Array(4).fill(null),
+        Array(4).fill(null)
+    ];
+
+    // Notes:
+    // Based on my understanding, there are a total of 16 dice on a 4x4 board
+    // Each die contains a letter and the user can only move to adjacent die, including diags
+    // This likely means that my board is actually an array of 4 arrays with 4 data elements
+    // 
+    // this component will need to check the target and verify they connect on each click (pass in x,y eg. Pos 2, Row 1 and use that as the outbound click?)
+    // it will also ideally keep track of the previous clicked die in case we decide to un-click it and unwind the word
+    // this will kick the composite word out on click assuming the word passes checks the scoring and submission pieces handle the rest
     return (
         <div>
 
